dfadd870a3f6f727da74eff4f04fd294f1a90283
Roderick Fung <roderickfung@gmail.com>
2016-08-02 09:42:44 -0700

    Question 4

---
+++ q4.js
@@ -0,0 +1,13 @@
+// 4. Write a JavaScript function called: isPrime. The function must take a number n and it should return `true` or `false` whether the argument passed (n) is a prime number or not. A prime number is a number that is divisible only by 1 and itself.
+
+var isPrime = function (n) {
+  for (var i=2; i<n; i++){
+    if (n % i === 0) {
+      return false
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(44));
+console.log(isPrime(7));
